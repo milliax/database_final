bc59f49bd3901f399ce40432f98450990810f8c2
milliax <maxchengboy@gmail.com>
2025-06-04 19:35:17 +0800

    add prisma generate to package json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+            },{
+                protocol: 'https',
+                hostname: 'localhost'
+            }
+        ]
+    }
 };
 
 export default nextConfig;
