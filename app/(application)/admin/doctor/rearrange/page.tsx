f8e4a80f6b5bb085a92260e08f67e3bfa04f68a8
milliax <maxchengboy@gmail.com>
2025-06-07 18:22:58 +0800

    update menu content position

--- app/(application)/admin/doctor/rearrange/page.tsx
+++ app/(application)/admin/doctor/rearrange/page.tsx
@@ -1,8 +1,8 @@
 export default function AdminRearrangePage() {
     return (
         <div>
-            <h1 className="text-2xl font-bold text-center my-8">病歷紀錄</h1>
-            <p className="text-center text-gray-600">這裡將顯示病歷紀錄的相關資訊。</p>
+            <h1 className="text-2xl font-bold text-center my-8">代班</h1>
+            <p className="text-center text-gray-600">依照醫生請假安排代班</p>
             {/* 這裡可以放置更多的病歷紀錄內容 */}
         </div>
     );
