f109b59f8062cfcef94d0c2bf12576fcdc504a58
qwerty.mg11@nycu.edu.tw <qwerty.mg11@nycu.edu.tw>
2025-06-08 22:22:35 +0800

    commit message

--- app/(application)/doctor/page.tsx
+++ app/(application)/doctor/page.tsx
@@ -1,6 +1,6 @@
 export default function DoctorSummaryPage() {
     return (
-        <div>
+        <div classsName="container mx-auto p-4">
             <h1>Doctor Summary</h1>
             <p>This is the summary page for the doctor.</p>
         </div>
